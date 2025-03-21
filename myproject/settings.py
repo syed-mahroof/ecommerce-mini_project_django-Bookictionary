162b988541aa40316a8476e2de6ebc44b8d9ab96
syed-mahroof <syedmahroof2002@gmail.com>
2025-03-21 12:40:25 +0530

    4 deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR / "static")]
 
 
